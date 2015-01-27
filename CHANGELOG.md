fcea40ac42859efa594ccadabf067a840a9f865f
Suhayb El Wardany <me@suw.me>
2015-01-27 05:25:50 +0200

    update info files, psr-2
    Signed-off-by: Suhayb El Wardany <me@suw.me>

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# Testing Change Log
+
+This project follows [Semantic Versioning](CONTRIBUTING.md).
+
+## Proposals
+
+We do not give estimated times for completion on `Accepted` Proposals.
+
+- [Accepted](https://github.com/cartalyst/testing/labels/Accepted)
+- [Rejected](https://github.com/cartalyst/testing/labels/Rejected)
+
+---
+
+### v1.0.0 - 2015-01-27
+
+- Added `IlluminateTestCase` that defines an IoC container with mocks for common used classes.
--- src/IlluminateTestCase.php
+++ src/IlluminateTestCase.php
@@ -1,4 +1,5 @@
-<?php namespace Cartalyst\Testing;
+<?php
+
 /**
  * Part of the Testing package.
  *
@@ -17,83 +18,84 @@
  * @link       http://cartalyst.com
  */
 
+namespace Cartalyst\Testing;
+
 use Mockery as m;
 use PHPUnit_Framework_TestCase;
 use Illuminate\Container\Container;
 use Illuminate\Support\Facades\Facade;
 
-class IlluminateTestCase extends PHPUnit_Framework_TestCase {
+class IlluminateTestCase extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Close mockery.
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        m::close();
+    }
 
-	/**
-	 * Close mockery.
-	 *
-	 * @return void
-	 */
-	public function tearDown()
-	{
-		m::close();
-	}
+    /**
+     * Setup.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        // IoC Container
+        $this->app = new Container;
 
-	/**
-	 * Setup.
-	 *
-	 * @return void
-	 */
-	public function setUp()
-	{
-		// IoC Container
-		$this->app = new Container;
+        // IoC Bindings
+        $this->app['alerts']     = m::mock('Cartalyst\Alerts\Alerts');
+        $this->app['cache']      = m::mock('Illuminate\Cache\CacheManager');
+        $this->app['config']     = m::mock('Illuminate\Config\Repository');
+        $this->app['datagrid']   = m::mock('Cartalyst\DataGrid\DataGrid');
+        $this->app['events']     = m::mock('Illuminate\Events\Dispatcher');
+        $this->app['files']      = m::mock('Illuminate\Filesystem\Filesystem');
+        $this->app['redirect']   = m::mock('Illuminate\Routing\Redirector');
+        $this->app['request']    = m::mock('Illuminate\Http\Request');
+        $this->app['sentinel']   = m::mock('Cartalyst\Sentinel\Sentinel');
+        $this->app['session']    = m::mock('Illuminate\Session\SessionManager');
+        $this->app['translator'] = m::mock('Illuminate\Translation\Translator');
+        $this->app['url']        = m::mock('Illuminate\Routing\UrlGenerator');
+        $this->app['validator']  = m::mock('Illuminate\Validation\Factory');
+        $this->app['view']       = m::mock('Illuminate\View\Factory');
 
-		// IoC Bindings
-		$this->app['alerts']     = m::mock('Cartalyst\Alerts\Alerts');
-		$this->app['cache']      = m::mock('Illuminate\Cache\CacheManager');
-		$this->app['config']     = m::mock('Illuminate\Config\Repository');
-		$this->app['datagrid']   = m::mock('Cartalyst\DataGrid\DataGrid');
-		$this->app['events']     = m::mock('Illuminate\Events\Dispatcher');
-		$this->app['files']      = m::mock('Illuminate\Filesystem\Filesystem');
-		$this->app['redirect']   = m::mock('Illuminate\Routing\Redirector');
-		$this->app['request']    = m::mock('Illuminate\Http\Request');
-		$this->app['sentinel']   = m::mock('Cartalyst\Sentinel\Sentinel');
-		$this->app['session']    = m::mock('Illuminate\Session\SessionManager');
-		$this->app['translator'] = m::mock('Illuminate\Translation\Translator');
-		$this->app['url']        = m::mock('Illuminate\Routing\UrlGenerator');
-		$this->app['validator']  = m::mock('Illuminate\Validation\Factory');
-		$this->app['view']       = m::mock('Illuminate\View\Factory');
+        // Configurations
+        $this->app['config']->shouldIgnoreMissing();
 
-		// Configurations
-		$this->app['config']->shouldIgnoreMissing();
+        // Set the facade container
+        Facade::setFacadeApplication($this->app);
+    }
 
-		// Set the facade container
-		Facade::setFacadeApplication($this->app);
-	}
+    /**
+     * Set trans expectation.
+     *
+     * @param  int  $times
+     * @return this
+     */
+    protected function trans($times = 1)
+    {
+        $this->app['translator']->shouldReceive('trans')
+            ->times($times);
 
-	/**
-	 * Set trans expectation.
-	 *
-	 * @param  int  $times
-	 * @return this
-	 */
-	protected function trans($times = 1)
-	{
-		$this->app['translator']->shouldReceive('trans')
-			->times($times);
+        return $this;
+    }
 
-		return $this;
-	}
+    /**
+     * Set a redirect method expectation.
+     *
+     * @param  $method  string
+     * @return this
+     */
+    protected function redirect($method)
+    {
+        $this->app['redirect']->shouldReceive($method)
+            ->once()
+            ->andReturn($this->app['redirect']);
 
-	/**
-	 * Set a redirect method expectation.
-	 *
-	 * @param  $method  string
-	 * @return this
-	 */
-	protected function redirect($method)
-	{
-		$this->app['redirect']->shouldReceive($method)
-			->once()
-			->andReturn($this->app['redirect']);
-
-		return $this;
-	}
-
+        return $this;
+    }
 }
